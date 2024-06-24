2816d4da70467b59e36c108623a01731bcbffe95
Shobhit_mahajan <88932500+shobhit-mahajan@users.noreply.github.com>
2024-06-24 12:34:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,9 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
     <div className='w-full md:w-10/12 m-auto font-sans'>
       <App/>
    
     </div>
-  </React.StrictMode>
 )
